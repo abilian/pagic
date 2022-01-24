11c4283fa2d757d4e715240a4dc314c0a23aa881
Stefane Fermigier <sf@abilian.com>
2022-01-24 17:57:53 +0100

    cleanup.

--- src/pagic/view_model.py
+++ src/pagic/view_model.py
@@ -1,8 +1,6 @@
-import datetime
 import traceback
 from typing import Any
 
-import pendulum
 from attrs import define
 
 
@@ -28,32 +26,13 @@ class ViewModel:
             return self._extra_attrs[key]
 
         value = getattr(self._model, key)
-        if isinstance(value, datetime.datetime):
-            # Hack
-            return datetime_to_pendulum(value)
-        else:
-            return value
+        return value
 
     def extra_attrs(self):
         return {}
 
     def _unwrap(self):
         return self._model
-
-
-def datetime_to_pendulum(value):
-    dt = value
-    pendulum_value = pendulum.datetime(
-        dt.year,
-        dt.month,
-        dt.day,
-        dt.hour,
-        dt.minute,
-        dt.second,
-        0,
-        tz=dt.tzinfo,
-    )
-    return pendulum_value
 
 
 def unwrap(obj):
